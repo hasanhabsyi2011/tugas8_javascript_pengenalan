29969957831915ec408cfb7206a5eff6e9ea0e73
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-09-20 22:02:04 +0700

    Add files via upload

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+var motor={
+  type: 'matic',
+  harga: 18000000,
+  warna: 'putih'
+}
+
+console.log(motor);
+
+motor.warna = 'hitam';
+
+console.log(motor);
+console.log("yang berubah: ",motor.warna);
